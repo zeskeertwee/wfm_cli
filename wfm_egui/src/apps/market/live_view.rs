bbdcba0423f2613c177105ffaaf9127fcfe30878
Jasper Fortuin <zeskeertwee@gmail.com>
2023-12-18 16:14:23 +0100

    Fix errors after merging

--- wfm_egui/src/apps/market/live_view.rs
+++ wfm_egui/src/apps/market/live_view.rs
@@ -1,4 +1,4 @@
-use eframe::egui::{CtxRef, Ui};
+use eframe::egui::{Context, Ui};
 use wfm_rs::websocket::{WebsocketMessagePayload, WebsocketOrder};
 use wfm_rs::shared::OrderType;
 use crate::app::{App, AppWindow, WFM_WS_EVENTS_KEY};
@@ -14,7 +14,7 @@ impl AppWindow for LiveMarketApp {
         "Live warframe.market orders".to_string()
     }
 
-    fn update(&mut self, app: &App, ctx: &CtxRef, ui: &mut Ui) {
+    fn update(&mut self, app: &App, ctx: &Context, ui: &mut Ui) {
         if app.present_in_storage(WFM_WS_EVENTS_KEY) {
             app.get_from_storage::<Vec<WebsocketMessagePayload>, _, _>(WFM_WS_EVENTS_KEY, |v| v.unwrap().iter().map(|v| match v {
                 WebsocketMessagePayload::NewOrder { order } => match order.order_type {
